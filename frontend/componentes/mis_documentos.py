23cab28ecb2a8c6502991ec09e91248991617ab5
RamiroSabetta <155183794+RamiroSabetta@users.noreply.github.com>
2025-07-01 21:45:51 -0300

    cambios. agrego descripcion, filtro

--- frontend/componentes/mis_documentos.py
+++ frontend/componentes/mis_documentos.py
@@ -1,10 +1,15 @@
 from nicegui import ui
 
+ESTILOS_DESCARGAS = 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800'
+
 class mis_documentos :
     def __init__ (self, archivos) :
         
         self.archivos = archivos
-        self.arbol = ui.tree(self.archivos, label_key='nombre', on_select=lambda e: ui.notify(e.value))
+        self.arbol = ui.tree(self.archivos, label_key='nombre')
+        
+        self.arbol.add_slot('default-body', f''' <span :props="props">Descripción: {{ props.node.descripcion }}</span> <button class="{ESTILOS_DESCARGAS}">Hola</button> ''')
+        ui.input('filtrar').bind_value_to(self.arbol, 'filter')
     
     def get_arbol(self) :
         return self.arbol
--- frontend/componentes/test_componentes.py
+++ frontend/componentes/test_componentes.py
@@ -4,16 +4,16 @@ from mis_documentos import mis_documentos
 
 archivos = [
     
-    {'id': '1', 'nombre': 'Archivos de programación',
+    {'id': '1', 'nombre': 'Archivos de programación', 'descripcion' : 'Directorio que contiene archivos de programación',
      
     'children': 
     
-        [{'id': '1A', 'nombre' : 'holaMundo.js'}, 
+        [{'id': '1A', 'nombre' : 'holaMundo.js', 'descripcion' : 'Un archivo JS'}, 
         
-        {'id': '2A' , 'nombre' : 'hola.txt'}]},
+        {'id': '2A' , 'nombre' : 'hola.txt', 'descripcion' : 'Un archivo TXT'}]},
     
     
-    {'id': '2', 'nombre': 'Fotos', 
+    {'id': '2', 'nombre': 'Fotos', 'descripcion' : 'Directorio que contiene fotos',
 
     'children': 
         
