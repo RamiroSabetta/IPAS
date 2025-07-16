3b2d6de5ae2a82f30fe52323c9f8dd982f02ae7e
RamiroSabetta <155183794+RamiroSabetta@users.noreply.github.com>
2025-07-15 21:48:12 -0300

    cambios en login, agg nuevo diseño

--- frontend/login.py
+++ frontend/login.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 from typing import Optional
 import requests
 from fastapi import Request
@@ -73,19 +72,26 @@ def login(redirect_to: str = '/') -> Optional[RedirectResponse]:
     
     if app.storage.user.get('authenticated', False):
         return RedirectResponse('/')
-    with ui.row().classes('justify-center items-center self-center pt-14'):
-        ui.label('IPAS').classes('text-4xl italic')
-        ui.image('./frontend/assets/logo.png').classes('w-20')
+    
         
-    with ui.card().classes('absolute-center gap-10'):
+    with ui.card().classes('absolute-center gap-8 w-[450px] h-fit'):
         
-        with ui.column().classes('self-start'):
-            username = ui.input('Nombre de usuario', validation={'El valor del campo excede el máximo de carácteres': lambda value: len(value) < 50}).on('keydown.enter', try_login)
-            password = ui.input('Contraseña', password=True, password_toggle_button=True).on('keydown.enter', try_login)
+        with ui.row().classes('justify-center items-center self-center pt-5'):
+            ui.label('IPAS').classes('text-4xl italic')
+            ui.image('./frontend/assets/logo.png').classes('w-20')
+            
         
-        with ui.column().classes('items-center self-center') as contenedorBotones:
-            ui.button('INICIAR SESIÓN', on_click=try_login)
-            ui.button('Olvidé mis datos', on_click=lambda: ui.notify('Para recuperar tu nombre de Usuario y/o Contraseña contacta a un Administrador', close_button='OK', position='top'))
+        ui.separator().classes('w-full')
+        
+        
+        with ui.column().classes('items-center self-center'):
+            username = ui.input('Nombre de usuario', validation={'El valor del campo excede el máximo de carácteres': lambda value: len(value) < 50}).on('keydown.enter', try_login).classes('w-full')
+            password = ui.input('Contraseña', password=True, password_toggle_button=True).on('keydown.enter', try_login).classes('w-full')
+        
+        with ui.column().classes('items-center self-center pb-5') as contenedorBotones:
+            ui.button('INICIAR SESIÓN', on_click=try_login).classes('w-full')
+            ui.button('Olvidé mis datos', on_click=lambda: ui.notify('Para recuperar tu nombre de Usuario y/o Contraseña contacta a un Administrador', close_button='OK', position='top')).classes('w-full')
+            
     return None
 
 if __name__ in {'__main__', '__mp_main__'}:
